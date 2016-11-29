fd7eba07dc900223c026dea118886eebf3379abe
dilo00o <mare.instalator@gmail.com>
2016-11-29 15:15:16 +0100

    small refact

--- break_encryption.py
+++ break_encryption.py
@@ -15,23 +15,23 @@ def read_cyphers(team):
     return ciphers
 
 
-ciphers = read_cyphers(1)
+ciphers = read_cyphers(2)
 current_messages = []
 shortest = 0
 mode = "cribGuess"  # or cribFollow
 tempDecryptedText = ""
 
 # Totally decrypted:
-CJPlain = "Trust I seek and I find in you\n'Cause you know I'm here for you\nBody's aching all the time\nReaching a fever pitch\nIgnite the light\nTo seize everything you ever wanted\nIt's such a feelin' that my love\nYou're in control just like a child\nMy worst distraction, my rhythm and blues\n"
-CIPlain = "UHlelo Lobhalomfihlo kuyikhono ukukhuluma phambi sophikisana. Lokhu isayensi kuyisisekelo of security computer zanamuhla. Abacwaningi Security ukudala ezokuphepha ubuchule. Kukhona ubuchule eziningi ezinjalo, owaziwa kakhulu kuba ukubethela. Ukubethela isinika ithuba ukudlulisa ulwazi oluyimfihlo."
+CAPlain = "Vienna is the capital of Austria. It is one of its largests cities.\nVienna is 410 square kilometers, almost 2 million people live there.\nVienna is situated in the Eastern part of Austria, it is standing on Danube river.\nVienna is in the UTC+1 time zone.\nWhen tourists come to Vienna they often visit  Sch\xc3\xb6nbrunn Palace, Austrian Parliament,\nCity Hall, and St. Stephen's Cathedral.\nDo not forget to go to opera when you visit Vienna.\n"
+CBPlain = "Shame on you! Shame, shame, shame!\nWhat are you trying to do? Are you trying to break my cryptosystem?\nDo you know that reading other people's letters is not very nice and not polite?\nI am not doing that knind of things!\nWho on earth gets into my personal life like that? And why are you doing it?\nIs someone forcing you? No? If not, then what the hell is wrong with you?\nMy messages to Alice are strictly confidential! Only me, Alice and the NSA can read this e-mails..\nSo, dear hacker, stop it right now!\nBest regards,\nEve.\n"
+CCPlain = "Man, cartoons are the best! I like Futurama.\nIt is a sci-fi show about future. The story turns around a guy from the 20th century\nwho was frozen for many years and then he wakes up in a crazy future!\nThey have created 7 seasons and every one of them is just great!\nThey have so many fun characters, professor Farnsworth, Doctor Zoiberg, Leela\nand a robot called Bender! You should watch it!\n"
 CDPlain = "Scholars agree that classical information are an interesting new topic in the field of e-voting technology, and electrical engineers concur. In this work, we prove the visualization of information retrieval systems. In order to fix this obstacle, we verify not only that Scheme and Markov models are largely incompatible, but that the same is true for the World Wide Web. \n"
 CEPlain = "Leading analysts agree that electronic communication are an interesting new topic in the field of theory, and cyberinformaticians concur. After years of confusing research into scatter/gather I/O, we prove the visualization of write-ahead logging. Of course, this is not always the case. Moth, our new application for voice-over-IP, is the solution to all of these obstacles. \n"
-CCPlain = "Man, cartoons are the best! I like Futurama.\nIt is a sci-fi show about future. The story turns around a guy from the 20th century\nwho was frozen for many years and then he wakes up in a crazy future!\nThey have created 7 seasons and every one of them is just great!\nThey have so many fun characters, professor Farnsworth, Doctor Zoiberg, Leela\nand a robot called Bender! You should watch it!\n"
 CFPlain = "The implications of multimodal algorithms have been far-reaching and pervasive. Given the current status of concurrent technology, information theorists obviously desire the deployment of replication, which embodies the private principles of cyberinformatics. In this position paper we introduce a novel application for the analysis of RAID (Tamkin), proving that Internet QoS and IPv4 can collaborate to fulfill this objective. \n"
-CAPlain = "Vienna is the capital of Austria. It is one of its largests cities.\nVienna is 410 square kilometers, almost 2 million people live there.\nVienna is situated in the Eastern part of Austria, it is standing on Danube river.\nVienna is in the UTC+1 time zone.\nWhen tourists come to Vienna they often visit  Sch\xc3\xb6nbrunn Palace, Austrian Parliament,\nCity Hall, and St. Stephen's Cathedral.\nDo not forget to go to opera when you visit Vienna.\n"
+CIPlain = "UHlelo Lobhalomfihlo kuyikhono ukukhuluma phambi sophikisana. Lokhu isayensi kuyisisekelo of security computer zanamuhla. Abacwaningi Security ukudala ezokuphepha ubuchule. Kukhona ubuchule eziningi ezinjalo, owaziwa kakhulu kuba ukubethela. Ukubethela isinika ithuba ukudlulisa ulwazi oluyimfihlo."
 CGPlain = "Local-area networks and autonomous epistemologies have been extensively synthesized by cyberneticists. It should be noted that Gob controls hierarchical databases. Existing mobile and real-time heuristics use client-server modalities to explore large-scale theory. While conventional wisdom states that this issue is generally addressed by the development of the Internet, we believe that a different method is necessary. In the opinions of many, we view networking as following a cycle of four phases: refinement, creation, simulation, and creation. Thus, we concentrate our efforts on proving that the partition table and e-business are generally incompatible. \n"
-CBPlain = "Shame on you! Shame, shame, shame!\nWhat are you trying to do? Are you trying to break my cryptosystem?\nDo you know that reading other people's letters is not very nice and not polite?\nI am not doing that knind of things!\nWho on earth gets into my personal life like that? And why are you doing it?\nIs someone forcing you? No? If not, then what the hell is wrong with you?\nMy messages to Alice are strictly confidential! Only me, Alice and the NSA can read this e-mails..\nSo, dear hacker, stop it right now!\nBest regards,\nEve.\n"
 CHPlain = "Мать с младенцем спасена;\nЗемлю чувствует она.\nНо из бочки кто их вынет?\nБог неужто их покинет?\nСын на ножки поднялся,\nВ дно головкой уперся,\nПонатужился немножко:\n\"Как бы здесь на двор окошко\nНам проделать?\" - молвил он,\nВышиб дно и вышел вон.\nМать и сын теперь на воле;\nВидят холм в широком поле;\nМоре синее кругом,\nДуб зеленый над холмом.\nСын подумал: добрый ужин\nБыл бы нам, однако, нужен.\nЛомит он у дуба сук\nИ в тугой сгибает лук,\nСо креста снурок шелковый\nНатянул на лук дубовый,\nТонку тросточку сломил,\nСтрелкой легкой завострил\nИ пошел на край долины\nУ моря искать дичины.\n"
+CJPlain = "Trust I seek and I find in you\n'Cause you know I'm here for you\nBody's aching all the time\nReaching a fever pitch\nIgnite the light\nTo seize everything you ever wanted\nIt's such a feelin' that my love\nYou're in control just like a child\nMy worst distraction, my rhythm and blues\n"
 
 
 def str_xor(a, b):  # xor two strings of different lengths
@@ -208,48 +208,58 @@ def root_of_all_decryption():
                     crib = tempDecryptedText + crib
                 temp = []  # len = shortest cipher
                 toPrint = []
-                for index in range(shortest - len(crib) + 1):
-                    guesses = []  # len = len(ciphers)
-                    printed = True
-                    for k in range(len(ciphers)):
-                        if cipherNb != k:  # pas de xor avec sois-meme
-                            xored = str_xor(ciphers[cipherNb].decode("base64"), ciphers[k].decode("base64"))
-                            guess = str_xor(xored[index:index + len(crib)], crib)
-                            guesses.append(guess)
-                            if not is_good_guess_hard(guess):
-                                printed = False
-                        else:
-                            guesses.append(crib)
-                    temp.append(guesses)
-                    if printed:
-                        toPrint.append(True)
-                    else:
-                        toPrint.append(False)
+                extracte_something_from_the_really_long_mthd(cipherNb, crib, temp, toPrint)
                 print_guess(temp, toPrint)
                 print("")
                 choice = raw_input(
                     "Enter the matched position, 'none' for no match, 'switch' to switch to the other decryption mode, or 'end' to quit: ").strip(
                     " ")
                 if choice != "none" and choice != "end" and choice != "switch":
-                    position = int(choice)
-                    for index in range(len(current_messages)):
-                        crib_length = len(temp[position][index])
-                        current_messages[index] = current_messages[index][:position] + temp[position][index] + \
-                                                  current_messages[index][position + crib_length:]
-                    if mode == "cribFollow":
-                        tempDecryptedText = crib
-                    if is_decrypted(cipherNb):
-                        choice = 'end'
-                        print("THE SHORTEST MESSAGE HAS BEEN DECRYPTED HUZZAH")
+                    choice = valid_choice(choice, cipherNb, crib, temp)
                 elif choice == "switch":
                     switch_mode()
                 print("The current messages are:\n\n")
                 print_messages(current_messages, 128)
-                print''
+                print()
             except (NameError, SyntaxError):
                 print("Don't forget to write your crib between \" \"")
         else:
             print("Incorrect message number, try again")
+
+
+def valid_choice(choice, cipherNb, crib, temp):
+    global tempDecryptedText
+    position = int(choice)
+    for index in range(len(current_messages)):
+        crib_length = len(temp[position][index])
+        current_messages[index] = current_messages[index][:position] + temp[position][index] + \
+                                  current_messages[index][position + crib_length:]
+    if mode == "cribFollow":
+        tempDecryptedText = crib
+    if is_decrypted(cipherNb):
+        choice = 'end'
+        print("THE SHORTEST MESSAGE HAS BEEN DECRYPTED HUZZAH")
+    return choice
+
+
+def extracte_something_from_the_really_long_mthd(cipherNb, crib, temp, toPrint):
+    for index in range(shortest - len(crib) + 1):
+        guesses = []  # len = len(ciphers)
+        printed = True
+        for k in range(len(ciphers)):
+            if cipherNb != k:  # pas de xor avec sois-meme
+                xored = str_xor(ciphers[cipherNb].decode("base64"), ciphers[k].decode("base64"))
+                guess = str_xor(xored[index:index + len(crib)], crib)
+                guesses.append(guess)
+                if not is_good_guess_hard(guess):
+                    printed = False
+            else:
+                guesses.append(crib)
+        temp.append(guesses)
+        if printed:
+            toPrint.append(True)
+        else:
+            toPrint.append(False)
 
 
 if __name__ == '__main__':

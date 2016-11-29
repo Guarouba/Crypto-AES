0fc8fe9bd08f021e38d24d79d941317de08c2e8d
dilo00o <mare.instalator@gmail.com>
2016-11-29 16:50:19 +0100

    added partial translations

--- break_encryption.py
+++ break_encryption.py
@@ -15,7 +15,7 @@ def read_cyphers(team):
     return ciphers
 
 
-ciphers = read_cyphers(2)
+ciphers = read_cyphers(5)
 current_messages = []
 shortest = 0
 mode = "cribGuess"  # or cribFollow
@@ -28,9 +28,9 @@ CCPlain = "Man, cartoons are the best! I like Futurama.\nIt is a sci-fi show abo
 CDPlain = "Scholars agree that classical information are an interesting new topic in the field of e-voting technology, and electrical engineers concur. In this work, we prove the visualization of information retrieval systems. In order to fix this obstacle, we verify not only that Scheme and Markov models are largely incompatible, but that the same is true for the World Wide Web. \n"
 CEPlain = "Leading analysts agree that electronic communication are an interesting new topic in the field of theory, and cyberinformaticians concur. After years of confusing research into scatter/gather I/O, we prove the visualization of write-ahead logging. Of course, this is not always the case. Moth, our new application for voice-over-IP, is the solution to all of these obstacles. \n"
 CFPlain = "The implications of multimodal algorithms have been far-reaching and pervasive. Given the current status of concurrent technology, information theorists obviously desire the deployment of replication, which embodies the private principles of cyberinformatics. In this position paper we introduce a novel application for the analysis of RAID (Tamkin), proving that Internet QoS and IPv4 can collaborate to fulfill this objective. \n"
-CIPlain = "UHlelo Lobhalomfihlo kuyikhono ukukhuluma phambi sophikisana. Lokhu isayensi kuyisisekelo of security computer zanamuhla. Abacwaningi Security ukudala ezokuphepha ubuchule. Kukhona ubuchule eziningi ezinjalo, owaziwa kakhulu kuba ukubethela. Ukubethela isinika ithuba ukudlulisa ulwazi oluyimfihlo."
 CGPlain = "Local-area networks and autonomous epistemologies have been extensively synthesized by cyberneticists. It should be noted that Gob controls hierarchical databases. Existing mobile and real-time heuristics use client-server modalities to explore large-scale theory. While conventional wisdom states that this issue is generally addressed by the development of the Internet, we believe that a different method is necessary. In the opinions of many, we view networking as following a cycle of four phases: refinement, creation, simulation, and creation. Thus, we concentrate our efforts on proving that the partition table and e-business are generally incompatible. \n"
 CHPlain = "Мать с младенцем спасена;\nЗемлю чувствует она.\nНо из бочки кто их вынет?\nБог неужто их покинет?\nСын на ножки поднялся,\nВ дно головкой уперся,\nПонатужился немножко:\n\"Как бы здесь на двор окошко\nНам проделать?\" - молвил он,\nВышиб дно и вышел вон.\nМать и сын теперь на воле;\nВидят холм в широком поле;\nМоре синее кругом,\nДуб зеленый над холмом.\nСын подумал: добрый ужин\nБыл бы нам, однако, нужен.\nЛомит он у дуба сук\nИ в тугой сгибает лук,\nСо креста снурок шелковый\nНатянул на лук дубовый,\nТонку тросточку сломил,\nСтрелкой легкой завострил\nИ пошел на край долины\nУ моря искать дичины.\n"
+CIPlain = "UHlelo Lobhalomfihlo kuyikhono ukukhuluma phambi sophikisana. Lokhu isayensi kuyisisekelo of security computer zanamuhla. Abacwaningi Security ukudala ezokuphepha ubuchule. Kukhona ubuchule eziningi ezinjalo, owaziwa kakhulu kuba ukubethela. Ukubethela isinika ithuba ukudlulisa ulwazi oluyimfihlo."
 CJPlain = "Trust I seek and I find in you\n'Cause you know I'm here for you\nBody's aching all the time\nReaching a fever pitch\nIgnite the light\nTo seize everything you ever wanted\nIt's such a feelin' that my love\nYou're in control just like a child\nMy worst distraction, my rhythm and blues\n"
 
 
@@ -70,12 +70,10 @@ def print_messages(messages, line_length=100):
         text_len = len(value)
         for part in xrange(0, text_len, line_length):
             if part > text_len - line_length:
-                temp = []
-                temp.append(value[part:])
+                temp = [value[part:]]
                 print("     " + str(temp))
             else:
-                temp = []
-                temp.append(value[part:part + line_length])
+                temp = [value[part:part + line_length]]
                 print ("     " + str(temp))
 
 
@@ -198,31 +196,35 @@ def root_of_all_decryption():
     print_messages(current_messages, 128)
     print''
     while choice != "end":
-        cipherNb = int(raw_input("Enter message number: "))
-        if (cipherNb < len(ciphers)) and (cipherNb >= 0):
-            try:
-                crib = input(
-                    "Enter your crib (have to be between \" \"): ")  # use input() instead of raw_input() to be able to use "\n"
-                if mode == "cribFollow":
-                    tempDecryptedText = get_temp_decrypted_text(cipherNb)
-                    crib = tempDecryptedText + crib
-                temp = []  # len = shortest cipher
-                toPrint = []
-                extracte_something_from_the_really_long_mthd(cipherNb, crib, temp, toPrint)
-                print_guess(temp, toPrint)
-                print("")
-                choice = raw_input(
-                    "Enter the matched position, 'none' for no match, 'switch' to switch to the other decryption mode, or 'end' to quit: ").strip(
-                    " ")
-                if choice != "none" and choice != "end" and choice != "switch":
-                    choice = valid_choice(choice, cipherNb, crib, temp)
-                elif choice == "switch":
-                    switch_mode()
-                print("The current messages are:\n\n")
-                print_messages(current_messages, 128)
-                print()
-            except (NameError, SyntaxError):
-                print("Don't forget to write your crib between \" \"")
+        try:
+            cipher_nb = int(raw_input("Enter message number: "))
+
+            if (cipher_nb < len(ciphers)) and (cipher_nb >= 0):
+                try:
+                    crib = input(
+                        "Enter your crib (have to be between \" \"): ")  # use input() instead of raw_input() to be able to use "\n"
+                    if mode == "cribFollow":
+                        tempDecryptedText = get_temp_decrypted_text(cipher_nb)
+                        crib = tempDecryptedText + crib
+                    temp = []  # len = shortest cipher
+                    to_print = []
+                    extracte_something_from_the_really_long_mthd(cipher_nb, crib, temp, to_print)
+                    print_guess(temp, to_print)
+                    print("")
+                    choice = raw_input(
+                        "Enter the matched position, 'none' for no match, 'switch' to switch to the other decryption mode, or 'end' to quit: ").strip(
+                        " ")
+                    if choice not in ["none", "end", "switch"]:
+                        choice = valid_choice(choice, cipher_nb, crib, temp)
+                    elif choice == "switch":
+                        switch_mode()
+                    print("The current messages are:\n\n")
+                    print_messages(current_messages, 128)
+                    print()
+                except (NameError, SyntaxError):
+                    print("Don't forget to write your crib between \" \"")
+        except Exception:
+            print("You've entered a wrong number, repeat the process")
         else:
             print("Incorrect message number, try again")
 
